7a3b1ca8d631a52bd57dc981e1d1f39cfb5ab9ab
ting <ting04180@gmail.com>
2024-11-28 11:12:44 +0800

    關於我們樣式修正

--- src/pages/aboutUs.vue
+++ src/pages/aboutUs.vue
@@ -10,7 +10,7 @@
         />
       </div>
       <div class="col-md-6 px-5">
-        <h3 class="mt-4 fw-bold">關於Table Tennis</h3>
+        <h3 class="mt-2 mt-md-4 fw-bold">關於Table Tennis</h3>
         <h5 class="lh-base">
           歡迎光臨我們的桌球用品專賣店！我們專注於提供最優質、最全面的桌球用品，滿足各種水平的桌球愛好者，從初學者到專業選手。我們的店內擁有各大知名品牌的球拍、膠皮、球台、桌球專用鞋、服飾，以及各種訓練輔助器材，致力於為客戶提供專業的桌球裝備和優質的購物體驗。
         </h5>
@@ -18,7 +18,7 @@
     </div>
     <div class="row mt-5">
       <div class="col-md-6 order-md-1 order-2 px-5">
-        <h3 class="fw-bold">我們的服務項目</h3>
+        <h3 class="mt-2 mt-md-0 fw-bold">我們的服務項目</h3>
         <h5 class="lh-base">
           <strong>專業諮詢</strong>：
           我們的團隊由經驗豐富的桌球教練和專業人士組成，可以根據您的需求和打法特點，推薦適合的裝備，幫助您提升比賽表現。
@@ -54,7 +54,7 @@
         />
       </div>
       <div class="col-md-6 px-5">
-        <h3 class="fw-bold">我們的目標</h3>
+        <h3 class="mt-2 mt-md-0 fw-bold">我們的目標</h3>
         <h5 class="lh-base">
           我們的目標是為每一位桌球愛好者提供最貼心、專業的服務，無論您是剛剛起步還是已有豐富經驗，都能在這裡找到滿意的產品與支援。期待您的光臨，與我們一起在桌球的世界中進步、突破！
         </h5>
